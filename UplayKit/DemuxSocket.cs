b30927ea9068cdc56a00bd39b7342265518acc2a
SlejmUr <slejmur@protonmail.com>
2023-01-27 22:07:17 +0100

    Fixed issue here about returning null in Void

--- UplayKit/DemuxSocket.cs
+++ UplayKit/DemuxSocket.cs
@@ -99,7 +99,6 @@ namespace UplayKit
             catch (Exception ex)
             {
                 InternalEx.WriteEx(ex);
-                return null;
             }
         }
         #endregion
