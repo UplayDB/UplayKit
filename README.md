fda5b325df72fe8c6e1393b4b729f04d7e5e3c1a
SlejmUr <slejmur@protonmail.com>
2023-02-24 23:02:58 +0100

    Dmx to Main (#3)
    * dmx up now.
    Still need some time investment for it.
    But there you go!
    
    * Added PartyConnection
    More Request in OwnershipCon
    Init Req in PCBangCon
    VS Cleaned
    
    * will move out some of the repos so be aware!
    
    * slowly migrating to other RestClient
    
    * V3 is done by changing
    
    * Final push then removing files
    
    * only left is uplaykit (thats what we want)
    
    * 0.0.0.2
    Most of things  will change here but the
    
    * 0.0.0.3
    
    * Fixed issue here about returning null in Void
    
    * 0.0.0.4
    
    * 0.0.0.4 fixing, not finished but worth it
    
    * 0.0.0.4 Finalized
    Should work everything (Maybe issue with push that we doest send but should catch it)
    
    * Just Version Bump!
    Recently 0.0.0.4 Caused issues cus released multiple times (dumb me)
    So now use 0.0.0.5
    
    * testing commin in multiple repo
    
    * 0.0.0.6 from yesterday
    
    * 0.0.0.7 Update
    (To 138.2.0.10815 version)
    
    * 0.0.0.8 Update, not much but small
    
    * Updated to 0.0.0.9
    Fixed Reconnect issue
    
    * Update to 0.0.0.10

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
 # UplayKit
 C# Client/SDK to interact with Uplay/Ubisoft API's
 
-# Folders
-Protobufs (Uplay-Protobufs) => Generating .dll and provide a Libary for Applications.
-
-UplayKit => Communicating with dmx.upc.ubisoft.com backend.
-
-DecryptPackets => Decrypting the Base64 string (decrypt.txt) or the binary data (data.bin).
-
-UbiServices => Gathering Information from public-ubiservices.ubi, store.ubi, etc.
+UplayKit is communicating with dmx.upc.ubisoft.com backend.
 
 # Thanks
-Thanks claabs for his work do decode the stuff and wrote a basic implementation of Demux!
+Thanks claabs for his work to decode the stuff and wrote a basic implementation of Demux!
---
+++ UplayKit/InternalEx.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace UplayKit
+{
+    internal class InternalEx
+    {
+        public static void WriteEx(Exception ex)
+        {
+            string ToWrite = "\n";
+            ToWrite += DateTime.UtcNow.ToString("yyyy.MM.dd hh:mm:ss") + " (UTC) | " + DateTime.Now.ToString("yyyy.MM.dd hh:mm:ss") + " (LOCAL)";
+            ToWrite += $"\nMethod: {ex.TargetSite.Name}";
+            ToWrite += $"\nInner: {ex.InnerException}";
+            ToWrite += $"\nStackTrace: {ex.StackTrace}";
+            ToWrite += $"\nMessage: {ex.Message}";
+            ToWrite += $"\nSource: {ex.Source}";
+            ToWrite += $"\nHResult: {ex.HResult}";
+            ToWrite += $"\nHelpLink: {ex.HelpLink}";
+            ToWrite += $"\nTargetSite: {JsonConvert.SerializeObject(ex.TargetSite)}";
+            ToWrite += $"\nData: {JsonConvert.SerializeObject(ex.Data)}";
+            File.AppendAllText("UplayKit_Ex.txt", ToWrite);
+        }
+    }
+}
